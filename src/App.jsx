f6fda35ca151ffd7f4615878bcf57855f24cb819
prabhat.dutt <prabhatdutt95@gmail.com>
2025-07-08 18:34:37 +0530

    Sample Template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ padding: "1rem", fontFamily: "Arial" }}>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
